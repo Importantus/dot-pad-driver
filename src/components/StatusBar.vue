35e58c8c4d4f127a24754fd1a9d7b901a508e5c0
Importantus <importantus@mailbox.org>
2024-06-22 19:06:20 +0200

    ✨ Add colored bar to show current state

---
+++ src/components/StatusBar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useStore } from '../store';
+
+const store = useStore();
+</script>
+<template>
+    <div class="w-full h-2" :style="{
+        backgroundColor: store.state.color
+    }"></div>
+</template>
