4b98ce8dccda20341bf2caf1649f783d64da057f
Importantus <importantus@mailbox.org>
2024-06-22 19:02:56 +0200

    🚧 Create store with listeners to key events from backend

---
+++ src/store.ts
@@ -0,0 +1,57 @@
+import { defineStore } from 'pinia'
+import { UIState, UIStates } from '../shared/constants'
+import { getFaceLandmarker, loadFaceLandmarker } from './lib/facelandmarker';
+
+export enum Connection {
+    DISCONNECTED,
+    LOADING,
+    READY
+}
+
+export const useStore = defineStore({
+    id: 'main',
+    state: () => ({
+        connection: Connection.DISCONNECTED,
+        frontendLoaded: false,
+        state: UIStates.uninitialized,
+        initialized: false,
+        cameraId: JSON.parse(localStorage.getItem("cameraId") || "null"),
+    }),
+    actions: {
+        async init() {
+            if (this.initialized) {
+                return;
+            }
+            window.ipcRenderer.on("state-change", (_, state: any) => {
+                console.log("State change:", state);
+                this.state = state as UIState;
+            });
+
+            window.ipcRenderer.on("ready", () => {
+                console.log("Backend is ready.");
+                this.connection = Connection.READY;
+            });
+
+            window.ipcRenderer.on("board-connected", () => {
+                console.log("Board connected.");
+                this.connection = Connection.LOADING;
+            });
+
+            window.ipcRenderer.on("board-disconnected", () => {
+                console.log("Board disconnected.");
+                this.connection = Connection.DISCONNECTED;
+            });
+
+            if (getFaceLandmarker() === null) {
+                await loadFaceLandmarker();
+            }
+
+            this.initialized = true;
+            this.frontendLoaded = true;
+        },
+        setCameraId(cameraId: string) {
+            this.cameraId = cameraId;
+            localStorage.setItem("cameraId", JSON.stringify(cameraId));
+        }
+    },
+})
