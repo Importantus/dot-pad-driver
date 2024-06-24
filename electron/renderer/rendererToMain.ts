bbb2eca170465343997db451bcd0bc6357b698da
Importantus <importantus@mailbox.org>
2024-06-24 17:53:14 +0200

    🐛 Register renderer listener only once

--- electron/renderer/rendererToMain.ts
+++ electron/renderer/rendererToMain.ts
@@ -2,8 +2,14 @@ import { ipcMain } from "electron";
 import { handleFaceMove } from "../mouseControl/faceMove";
 // import { dispatch } from "../controller";
 
+let registered = false;
+
 export function registerRendererEvents() {
+    if (registered) return;
+
     ipcMain.on("face-move", handleFaceMove);
+
+    registered = true;
 
     // ipcMain.on("start-face-tracking", () => {
     //     console.log('Start eye tracking | ')
