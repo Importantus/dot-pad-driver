90b21f6f99f6d9bad8466f4f47145de4cbb1421b
Importantus <importantus@mailbox.org>
2024-07-03 16:25:28 +0200

    🐛 Fix bug where icons are not displayed after build

--- src/components/DotBar.vue
+++ src/components/DotBar.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { UIState, UIStates } from '../../shared/constants';
 import { AppWindowIcon, Mic, ScanEye } from 'lucide-vue-next';
-import DotComponent from './DotComponent.vue';
+import { useStore } from '../store';
+
+const store = useStore();
 
 const dots: UIState[] = [];
 
@@ -10,18 +12,23 @@ Object.keys(UIStates).forEach((key) => {
         dots.push(UIStates[key]);
 });
 
-const icons: { [key: string]: typeof AppWindowIcon } = {
-    browsercontrol: AppWindowIcon,
-    speechrecognition: Mic,
-    facetracking: ScanEye
-}
-
 </script>
 
 <template>
     <div class="flex w-full justify-between">
-        <div v-for="i in dots" :key="i.id">
-            <DotComponent :state="i" :icon="icons[i.id]" />
+        <div v-for="state in dots" :key="state.id">
+            <div class="flex flex-col gap-2 items-center transition-all duration-300 ease-in-out" :style="{
+            scale: store.state.id === state.id ? 1.2 : 1
+        }">
+                <div class="w-20 h-20 rounded-full flex justify-center items-center" :style="{
+            backgroundColor: state.color
+        }">
+                    <AppWindowIcon v-if="state.id === 'browsercontrol'" class="text-black" :size="30" />
+                    <Mic v-else-if="state.id === 'speechrecognition'" class="text-black" :size="30" />
+                    <ScanEye v-else-if="state.id === 'facetracking'" class="text-black" :size="30" />
+                </div>
+                <div class="text-center text-xs text-gray-400">{{ state.title }}</div>
+            </div>
         </div>
     </div>
 </template>
